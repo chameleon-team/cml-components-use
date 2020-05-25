9e0f633640f38932c53f8f6b1fa0b39d1903aef8
JiM-W <550116990@qq.com>
2020-05-25 10:44:27 +0800

    feat: 原生组件引用demo

---
+++ chameleon.config.js
@@ -0,0 +1,49 @@
+
+// 设置静态资源的线上路径
+const publicPath = '//www.static.chameleon.com/cml';
+// 设置api请求前缀
+const apiPrefix = 'https://api.chameleon.com';
+
+cml.config.merge({
+  templateLang: "cml",
+  templateType: "html",
+  platforms: ["web","weex","wx","alipay","baidu","qq"],
+  buildInfo: {
+    wxAppId: '123456'
+  },
+  wx: {
+    dev: {
+    },
+    build: {
+      apiPrefix
+    }
+  },
+  web: {
+    dev: {
+      analysis: false,
+      console: false,
+      isWrapComponent: false // 取消默认对组件的包裹
+    },
+    build: {
+      analysis: false,
+      publicPath: `${publicPath}/web/`,
+      apiPrefix,
+      isWrapComponent: false // 取消默认对组件的包裹
+    }
+  },
+  weex: {
+    dev: {
+      isWrapComponent: false // 取消默认对组件的包裹
+    },
+    build: {
+      publicPath: `${publicPath}/weex/`,
+      apiPrefix,
+      isWrapComponent: false // 取消默认对组件的包裹
+    },
+    custom: {
+      publicPath: `${publicPath}/wx/`,
+      apiPrefix
+    }
+  }
+})
+
